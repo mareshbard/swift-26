50b2704b50589acdb16f97ebc8b51114222234b9
Leticia Gomes <125154278+mareshbard@users.noreply.github.com>
2026-02-05 01:03:36 +0000

    day 1

---
+++ Sources/SwiftSandbox/a.swift
@@ -0,0 +1,14 @@
+var numbers = [
+    "Months": [1,2,3,4,5,6,7,8,9,10,11,12],
+    "Days": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+]
+
+// para escrever em outros arquivos colocar como função
+func a() {
+
+for key in numbers.keys {
+    numbers[key]?.sort(by: >)
+}
+
+print(numbers["Months"] ?? "")
+}
--- Sources/SwiftSandbox/main.swift
+++ Sources/SwiftSandbox/main.swift
@@ -1,1 +1,28 @@
-print("Hello, World!")
+func main() {
+
+let nome = "Leticia"
+let idade = 19
+print("Meu nome é: \(nome) e tenho \(idade) anos")
+
+var menu: [String: Int] = [
+    "Café" : 10,
+    "Pão" : 2
+]
+
+print(menu["Pão"] ?? "")
+
+// Subscripting a dictionary with a key returns an 
+// optional value, because a dictionary might 
+// not hold a value for the key that you use in the subscript.
+
+menu["Espaguete"] = 15
+print(menu["Espaguete"] ?? "")
+// To add a new key-value pair, assign a value to 
+// a key that isn’t yet a part of the dictionary.
+
+for (produto, preco) in menu { //especificando par
+    print("Mercadoria: \(produto)\nPreço: \(preco)")
+}
+a()
+}
+main()
