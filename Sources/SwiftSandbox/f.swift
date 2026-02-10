af83869f9bb1026605e4f9e002deaa4c784f30d3
Leticia Gomes <125154278+mareshbard@users.noreply.github.com>
2026-02-10 01:08:11 +0000

    Strings

--- Sources/SwiftSandbox/f.swift
+++ Sources/SwiftSandbox/f.swift
@@ -2,9 +2,17 @@ func usandoString(){
     let umaString = "Um texto entre aspas"
     let textao = """
     Podemos escrever assim para facilitar a leitura 
-    do código (entre 3 aspas)
+    do código (entre 3 aspas). Ao adicionar uma barra \ 
+    no final daquela linha, quebra o texto
     """
     print(umaString)
     print(textao)
+
+    let linhasComEspaco = """
+    primeira linha
+            segunda linha
+                    terceira linha
+    """
+    print(linhasComEspaco)
     
 }
